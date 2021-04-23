9b08ea427a1b6fcf702a0c29fb7d258427d1c304
Krish Singal <krish.singal2002@gmail.com>
2021-04-23 18:31:27 -0500

    summary statistics

--- dataTransformation.py
+++ dataTransformation.py
@@ -10,7 +10,13 @@ import datetime
 fieldnames = ['benchmark_name', 'status', 'correct', 'execution_time_seconds']
 dictList = list()
 
+stat_fields = ['category', 'accuracy']
+summary_stats = list()
 
+term_correct = 0
+term_total = 0
+nonterm_correct = 0
+nonterm_total = 0
 
 for root, dirs, files in os.walk("output"):
     path = root.split(os.sep)
@@ -27,6 +33,7 @@ for root, dirs, files in os.walk("output"):
 
             d = dict()
             d['benchmark_name'] = path[len(path)-1] + '/' + ".".join(splitted[:2])
+            type = path[len(path) - 1]
 
             if(result < 0 ):
                 d['status'] = 'timeout'
@@ -41,10 +48,10 @@ for root, dirs, files in os.walk("output"):
 
                 if 'True' in split_relevant[0]:
                     status = 'True'
-                    correct = 'yes' if path[len(path) -1] == 'termination' else 'no'
+                    correct = 'yes' if  type == 'termination' else 'no'
                 elif 'False' in split_relevant[0]:
                     status = 'False'
-                    correct = 'yes' if path[len(path) -1] == 'nontermination' else 'no'
+                    correct = 'yes' if type == 'nontermination' else 'no'
                 else:
                     status = 'None'
                     correct = 'no'
@@ -64,8 +71,28 @@ for root, dirs, files in os.walk("output"):
 
                 d['execution_time_seconds'] = timeTaken
 
+
+
+            if type == 'termination':
+                term_total+=1
+                if correct == 'yes':
+                    term_correct+=1
+            elif type == 'nontermination':
+                nonterm_total+=1
+                if correct == 'yes':
+                    nonterm_correct+=1
+
             print(d)
             dictList.append(d)
+
+
+s1 = dict()
+s1['category'] = 'termination'
+s1['accuracy'] = round(term_correct/term_total,5)
+
+s2 = dict()
+s2['category'] = 'nontermination'
+s2['accuracy'] = round(nonterm_correct/nonterm_total,5)
 
 
 with open('output.csv', 'w', newline='') as csvfile:
@@ -73,5 +100,12 @@ with open('output.csv', 'w', newline='') as csvfile:
     writer.writeheader()
     for d in dictList:
         writer.writerow(d)
+
+# write summary stats
+with open('summary.csv', 'w', newline='') as summary:
+    writer2 = csv.DictWriter(summary, fieldnames=stat_fields)
+    writer2.writeheader()
+    writer2.writerow(s1)
+    writer2.writerow(s2)
 
 
